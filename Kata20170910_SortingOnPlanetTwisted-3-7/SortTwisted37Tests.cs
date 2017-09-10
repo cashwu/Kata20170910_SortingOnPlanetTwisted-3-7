4bbbbed43f992c1a1ac57a2d16af3320c3300c7f
cash <cash@cashwu.com>
2017-09-10 20:55:55 +0800

    rollback test case and add 3, 4 test case

--- Kata20170910_SortingOnPlanetTwisted-3-7/SortTwisted37Tests.cs
+++ Kata20170910_SortingOnPlanetTwisted-3-7/SortTwisted37Tests.cs
@@ -15,15 +15,9 @@ namespace Kata20170910_SortingOnPlanetTwisted_3_7
         }
 
         [TestMethod]
-        public void input_3_7_should_return_7_3()
+        public void input_3_4_should_return_4_3()
         {
-            SortTwisted37ShouldBe(new[] { 7, 3 }, new[] { 3, 7 });
-        }
-
-        [TestMethod]
-        public void input_2_3_7_should_return_2_7_3()
-        {
-            SortTwisted37ShouldBe(new[] { 2, 7, 3 }, new[] { 2, 3, 7 });
+            SortTwisted37ShouldBe(new[] { 4, 3 }, new[] { 3, 4 });
         }
 
         private static void SortTwisted37ShouldBe(int[] expected, int[] array)
@@ -38,20 +32,9 @@ namespace Kata20170910_SortingOnPlanetTwisted_3_7
     {
         public int[] SortTwisted37(int[] array)
         {
-            var arr = new[] { 3, 7 };
-            if (array.Count(a => arr.Contains(a)) != 2)
-            {
-                return array;
-            }
-
-            if (array[0] == 3 && array[1] == 7)
+            if (array[0] == 3 && array[1] == 4)
             {
                 return new[] { array[1], array[0] };
-            }
-
-            if (array[1] == 3 && array[2] == 7)
-            {
-                return new[] { array[0], array[2], array[1] };
             }
 
             return array;
