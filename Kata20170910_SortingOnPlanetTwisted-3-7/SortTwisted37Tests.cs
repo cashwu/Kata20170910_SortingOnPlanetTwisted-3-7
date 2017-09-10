cde18e9cef483dd327798cfd8bd56868f130c65f
cash <cash@cashwu.com>
2017-09-10 20:34:55 +0800

    input 2,3 test case

--- Kata20170910_SortingOnPlanetTwisted-3-7/SortTwisted37Tests.cs
+++ Kata20170910_SortingOnPlanetTwisted-3-7/SortTwisted37Tests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Kata20170910_SortingOnPlanetTwisted_3_7
@@ -7,8 +8,24 @@ namespace Kata20170910_SortingOnPlanetTwisted_3_7
     public class SortTwisted37Tests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void input_2_3_should_return_2_3()
         {
+            SortTwisted37ShouldBe(new[] {2, 3}, new[] {2, 3});
+        }
+
+        private static void SortTwisted37ShouldBe(int[] expected, int[] array)
+        {
+            var kata = new Kata();
+            var acutal = kata.SortTwisted37(array);
+            CollectionAssert.AreEqual(expected, acutal);
+        }
+    }
+
+    public class Kata
+    {
+        public int[] SortTwisted37(int[] array)
+        {
+            return array;
         }
     }
 }
